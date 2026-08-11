8b324aaff1e004739108880d1ed74e689ebf3b03
day90455 <33572302+day90455@users.noreply.github.com>
2026-08-11 15:28:26 +0800

    Remove homepage links from components.js
    Removed homepage links section from the component.

--- js/components.js
+++ js/components.js
@@ -414,17 +414,6 @@ function showHomepage() {
                 你可以在這裡查詢坐騎、寵物、幻卡、樂譜、時裝等各種收藏品的取得來源。<br>
                 資料來自 <a href="https://ffxivcollect.com" target="_blank" rel="noopener">FFXIV Collect</a>，圖示來自 <a href="https://xivapi.com" target="_blank" rel="noopener">XIVAPI</a>，並翻譯為繁體中文顯示。
             </p>
-            <div class="homepage-links">
-                <a href="https://cycleapple.github.io/xiv-tc-toolbox/" target="_blank" rel="noopener">
-                    更多工具
-                </a>
-                <a href="https://discord.gg/X556xjySDG" target="_blank" rel="noopener">
-                    加入 Discord 社群
-                </a>
-                <a href="https://portaly.cc/thecy" target="_blank" rel="noopener">
-                    ☕ 支持作者
-                </a>
-            </div>
         </div>
     `;
 }
