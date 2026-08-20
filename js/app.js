098ed02d9db87e59091b43d3e065a6b94571bd2c
day90455 <33572302+day90455@users.noreply.github.com>
2026-08-20 11:43:58 +0800

    Remove default checked state for showNoSourceToggle
    Remove default checked state for showNoSourceToggle.

--- js/app.js
+++ js/app.js
@@ -224,7 +224,6 @@ async function init() {
 
     // Set up event listeners
     setupEventListeners();
-    elements.showNoSourceToggle.checked = true;
     
     // Set up progress exclude category callback
     onProgressExcludeCategoryChange = function(category) {
